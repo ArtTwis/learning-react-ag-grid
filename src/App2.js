dea5a910d1f71cf1c0ca679de272713954e430ea
twinkle-goyal <twinkle.goyal@hp.com>
2023-01-31 22:20:25 +0530

    Quickstart Tutorial for AG Grid Enterprise

---
+++ src/App2.js
@@ -0,0 +1,68 @@
+import { useState, useRef, useEffect, useMemo } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import 'ag-grid-enterprise';
+
+function App2() {
+  const gridRef = useRef();
+
+  const [rowData, setRowData] = useState();
+
+  //   const [columnDefs, setColumnDefs] = useState([
+  //     { field: 'athlete' },
+  //     { field: 'age' },
+  //     { field: 'country', rowGroup: true },
+  //     { field: 'year', rowGroup: true },
+  //     { field: 'date' },
+  //     { field: 'sport' },
+  //     { field: 'gold' },
+  //     { field: 'silver' },
+  //     { field: 'bronze' },
+  //     { field: 'total' },
+  //   ]);
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'athlete' },
+    { field: 'age' },
+    { field: 'country' },
+    { field: 'year' },
+    { field: 'date' },
+    { field: 'sport' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ]);
+
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+      enableRowGroup: true,
+    }),
+    []
+  );
+
+  //   Fetch rowData from API cal.......
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
+  return (
+    <div className='ag-theme-alpine' style={{ height: '100%' }}>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        animateRows={true}
+        defaultColDef={defaultColDef}
+        rowGroupPanelShow='always'
+      />
+    </div>
+  );
+}
+
+export default App2;
